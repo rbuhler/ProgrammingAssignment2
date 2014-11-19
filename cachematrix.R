d91d8331b14860cd8f20efb9244167805a07ccee
Rodrigo Buhler <rodrigo.buhler@ymail.com>
2014-11-19 00:52:17 -0200

    Final Release
    This is the final release already checked.

--- cachematrix.R
+++ cachematrix.R
@@ -4,14 +4,14 @@
 # from already executed calculations.
 
 # The function makeCacheMatrix creates a special "vector", wich
-# stores a matrix and a list of functions, as decribed below:
+# stores an informed matrix and a list of functions, as decribed below:
 # 1. set - sets a matrix to the vector and clear the cache;
 # 2. get - gets the current matrix;
 # 3. getsolve - gets the cached execution result;
 # 4. setsolve - sets the cache with an execution result.
-
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
+  
   set <- function(y) {
     x <<- y
     m <<- NULL
@@ -19,27 +19,27 @@ makeCacheMatrix <- function(x = matrix()) {
   get <- function() x
   setsolve <- function(solve) m <<- solve
   getsolve <- function() m
+  
   list(set = set, get = get,
        setsolve = setsolve,
        getsolve = getsolve)
 }
 
-
-# The function cacheSolve calculates the inverse matrix of
-# the matrix stored into the special "vector" makeCacheMatrix.
-# However, if first checks in the "vector" for an already
-# calcualted result, if so this is the returned result. 
-# Otherwise the inverse matrix is calculated and the result
-# is stored into the "vector" and returned as the function
-# execution result.
-
+# The function cacheSolve returns the inverse matrix of
+# an informed matrix on the special "vector "of makeCacheMatrix.
+# If a calculation is already executed the return is the 
+# cached result, otherwise the inverse matrix is calculated and
+# the result stored into the cache for future requests that may
+# apply, the function then returns this result.
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+  # Check for an already calculated result
   m <- x$getsolve()
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
+  
+  # Otherwise executes the calculation
   data <- x$get()
   m <- solve(data)
   x$setsolve(m)
